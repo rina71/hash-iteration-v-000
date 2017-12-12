3544334b4df814b78d94d76623d270a70256b9a6
Yurina Shin <yurinas@gmail.com>
2017-12-12 20:44:53 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,16 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley",
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
-  # add the code snippet here!
+winner = " "
+passengers.each{ |key,value|
+if key == :suite_a && value.start_with?("A")
+winner = value  }  # add the code snippet here!
+end
+winner
 end
